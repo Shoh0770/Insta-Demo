ba90f129a31a714e59838ebe019f0283f960662e
Ziloliddin <Zilolidddinkamolov@gmail.com>
2022-07-04 17:37:31 +0500

    Initial Commit

---
+++ Insta Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Insta Demo
+//
+//  Created by Macbook Pro 14 Inch on 7/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
